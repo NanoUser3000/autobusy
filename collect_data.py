8a2add58ac4eb23f92b9cb5776fde5e8eea1af05
innhousepl <114243157+innhousepl@users.noreply.github.com>
2024-02-20 16:41:57 +0100

    Setup logging, use it (in data collection)

--- collect_data.py
+++ collect_data.py
@@ -3,42 +3,69 @@ from dotenv import dotenv_values
 import requests
 import json
 from time import sleep # TODO: something else for precision
+import logging
+import logging.config
+
+with open('log_config.json','r') as f:
+    logging_config = json.load(f)
+logging.config.dictConfig(config=logging_config)
+logger = logging.getLogger('collect_data')
+
+# TODO: info about params
+logger.info("Starting the program")
+
+def create_extra(resp_json):
+    """ Helper to create details for logging. """
+    return {
+        'details': "The response json received:\n"
+                   + json.dumps(resp_json, indent=4),
+    }
 
 def handle_json(resp_json: dict) -> None|list:
-    """ There are 3 possibilities anticipated:
-        - successful call to api, result is a list
-        - result: "false", error: "...error message..."
-        - result: "...error message..."
+    # TODO: what does this do though
+    """ Handles the json response from the API:
+    - if the request was successful, returns the data
+     (a list of dictionaries with data about vehicle positions)
+    - if it was unsuccessful, logs the error and returns None.
 
-        Returns None if unsuccessful, list of data about vehicles if successful.
+    There are 2 possibilities anticipated:
+        - successful call to api, value under "result" key is a list
+        - result: "false", error: "...error message..."
+    Other cases are logged with level error or higher.
+
+    Returns None if unsuccessful, list of data about vehicles if successful.
     """
     if 'result' not in resp_json:
-        print("CRITICAL ERROR (contact devs): unexpected: no result field in response")
-        # TODO: dump response to file to investigate
-        return
+        logger.critical('Unexpected: no result field in response',
+                     extra=create_extra(resp_json))
+        return None
 
     result = resp_json['result']
 
     if isinstance(result, list):
-        print("SUCCESSFUL CALL TO API")
+        # SUCCESS!
         return result
 
     if not isinstance(result, str):
-        print("CRITICAL ERROR (contact devs): result neither a list nor a string")
-        return
+        logger.critical('Unexpected: result neither a list nor a string',
+                     extra=create_extra(resp_json))
+        return None
 
-    # result is a string
-    if result == "false":
-        if 'error' in resp_json:
-            print("ERROR:", resp_json['error'])
-        else:
-            print("CRITICAL ERROR (contact devs): unexpected: result = \false\", no error field")
-        return
-    # result is a string nad not a "false"
-    print("ERROR in result:", result)
+    assert isinstance(result, str)
 
+    if result == "false" and 'error' in resp_json:
+        # note: this happens, we don't really know why # TODO ?
+        logger.warning('API call failed with error message: \"%s\"',
+                        resp_json['error'])
+    elif result == "false":
+        logger.error('Unexpected (but not breaking):'
+                     + ' result \"false\", no error message',
+                     extra=create_extra(resp_json))
+    else:
+        logger.warning('API call failed with result \"%s\"', result)
+    return None
+    
 # TODO: typing.TextIO
-# TODO?: somehow don't parse the json too much?
 def dump(data_list: list, file) -> None:
     """ Dumps the data to a file as json lines.  """
     for data in data_list:
@@ -62,25 +89,28 @@ params = {
 
 # TODO: preflight? (HEAD, check if there is new)
 # no need if we get 10s right
-# https://stackoverflow.com/questions/3393612/run-certain-code-every-n-seconds 
+# https://stackoverflow.com/questions/3393612/run-certain-code-every-n-seconds
+# (TODO) Idea: check when the data changes to be with requests in the middle
 FILE_NAME = "data.jsonl"
 with open(FILE_NAME, 'a') as file:
     for i in range(3):
         if i > 0:
             sleep(10) # TODO: threading or scheduling + coordinate with retries
+            # TODO: 15s or what as in docs?
         data_list: None = None # not the right type: make sure this screams and then fix
         while data_list is None:
             # TODO: handle failed timeout
+            # headers? Ideally (?), know if the data has changed
+            # maybe post with params in body is worth a try?
             response = requests.get(url=POSITION_API_URL, params=params, timeout=5)
             j_response = response.json()
             data_list = handle_json(j_response)
             if data_list is None: # while true for clean break before sleep?
                 # print("Fail, retrying...")
                 sleep(1)
-
-        # if data_list is not None:
         assert data_list is not None
         dump(data_list, file)
+        logger.info("Dumped %d position records successfully", len(data_list))
 
 
 ### ideas for analyzing data
